68ddf696015bf73493dc2ebf1a418dbecce9baf2
Vladimir Ka <kansept@ya.ru>
2021-09-30 10:21:16 +0300

    lesson 66

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -27,12 +27,14 @@ let Users = (props) => {
           </div>
           <div>
             {u.followed 
-              ? <button disabled={props.followingInProgress.some(id => id === u.id)} onClick={() => { 
-                props.unfollow(u.id);
-              }}>Unfollow</button> 
-              : <button disabled={props.followingInProgress.some(id => id === u.id)} onClick={() => {
-                props.follow(u.id)
-              }}>Follow</button>
+              ? <button 
+                  disabled={props.followingInProgress.some(id => id === u.id)} 
+                  onClick={() => { props.unfollow(u.id) }}
+                >Unfollow</button> 
+              : <button 
+                  disabled={props.followingInProgress.some(id => id === u.id)} 
+                  onClick={() => { props.follow(u.id) }}
+                >Follow</button>
             }
           </div>
         </span>
